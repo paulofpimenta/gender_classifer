32fcca99cef6f4bd0a9bad545c0906d57620a5f8
Paulo Pimenta <paulo-fernando.pimenta@atos.net>
2023-08-28 23:18:37 +0200

    Back and front connected

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,32 @@
+import json
 from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000"
+]
 
-@app.get("/")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/",response_class=JSONResponse)
 def read_root():
-    return {"Hello": "World"}
+    response = {"Hello": "World"}
+    return response
+
 
 
 @app.get("/items/{item_id}")
