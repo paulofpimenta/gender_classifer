d625a7d8349d569e3f4dd898bfbfbc027efd800f
Paulo Pimenta <paulo-fernando.pimenta@atos.net>
2023-09-21 15:13:50 +0200

    Added cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,7 @@ origins = [
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8000",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
     "http://localhost:3000",
 ]
 
