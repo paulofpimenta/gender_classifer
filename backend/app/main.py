5f8871d18b336672de08cfe568ac64401c2b73f8
Paulo Pimenta <paulo-fernando.pimenta@atos.net>
2023-09-21 11:19:11 +0200

    Backend ssl attemp

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,16 @@
 import io
+from subprocess import Popen
 from typing import Union
 from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import torch
 from PIL import Image
-import uvicorn
 
 # append the path of the parent directory
 import os, sys
+
+import uvicorn
 
 from model.ConvModel import ConvNet
 from model.classifer import ConvolutionalNeuralNet
@@ -67,15 +69,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-
 if __name__ == '__main__':
-    uvicorn.run("main:app",
-                host="0.0.0.0",
-                port=8000,
-                reload=True,
-                ssl_keyfile="/etc/letsencrypt/live/app1.ouicodedata.com/privkey.pem", 
-                ssl_certfile="/etc/letsencrypt/live/app1.ouicodedata.com/fullchain.pem"
-                )
-    
-#CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000",
+    Popen(['python', '-m', 'https_redirect'])  # Add this
+    uvicorn.run(
+        'main:app', port=443, host='0.0.0.0',
+        reload=True, reload_dirs=['html_files'],
+        ssl_keyfile='/etc/letsencrypt/live/app1.ouicodedata.com/privkey.pem',
+        ssl_certfile='/etc/letsencrypt/live/app1.ouicodedata.com/fullchain.pem')
